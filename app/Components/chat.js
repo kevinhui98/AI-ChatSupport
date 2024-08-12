2c736ff810feef4d5cb71c9661a51656698e0e60
dpatel12 <danyaalp99@gmail.com>
2024-08-11 20:55:27 -0700

    Prompt and setup for react

--- app/Components/chat.js
+++ app/Components/chat.js
@@ -1,5 +1,7 @@
 import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import ReactDom from 'react-dom'
 
 const chat = ({ messages }) => {
     return (
